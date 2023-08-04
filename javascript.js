9a2a3a8ead2a1130d98befe14c3eea80dd874705
k4tado <kylefurtado34@gmail.com>
2023-08-04 16:15:26 -0400

    Begin testing of program

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const squareElement = document.createElement('div');
+const grid = document.getElementById('container');
+squareElement.classList.add( "square" );
+
+grid.appendChild(squareElement);
