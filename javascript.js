684ca516f7e694440f7b80073ff185cf005eb433
k4tado <kylefurtado34@gmail.com>
2023-11-29 22:34:21 -0500

    Update

--- javascript.js
+++ javascript.js
@@ -2,4 +2,9 @@ const squareElement = document.createElement('div');
 const grid = document.getElementById('container');
 squareElement.classList.add( "square" );
 
+for (i = 0; i < 5; I++) {
+    
+}
+
+grid.appendChild(squareElement);
 grid.appendChild(squareElement);
